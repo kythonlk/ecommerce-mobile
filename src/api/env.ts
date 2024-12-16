d8a32ccab316c45bbd8a781ad3179e264577f142
Kythonlk <109597262+kythonlk@users.noreply.github.com>
2024-12-16 12:17:36 +0400

    Update env.ts

--- src/api/env.ts
+++ src/api/env.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    woocommerceApiUrl: 'https://dev.dhabione.com/wp-json/wc/v3/',
-    woocommerceConsumerKey: 'ck_de0ef375c0806f6121416a25f1c234fb573f74a6',
-    woocommerceConsumerSecret: 'cs_64f66e0bef363a9b21798da2dc23792cd9f0a3c7'
+    woocommerceApiUrl: '...',
+    woocommerceConsumerKey: 'ck_de....',
+    woocommerceConsumerSecret: 'cs_64...'
   };
   
